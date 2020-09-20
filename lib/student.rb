f6078ebe96b374869c85c8a5749d48210b3a352d
Michael Nnaji <mnnaji3@uwo.ca>
2020-09-20 16:39:55 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,6 +1,24 @@
 class Student
 
   # Remember, you can access your database connection anywhere in this class
-  #  with DB[:conn]  
-  
+  #  with DB[:conn]
+attr_accessor :name,:age 
+attr_reader :id
+def initialize(name,grade,id=nil)
+  @name = name
+  @grade = grade 
 end
+
+def self.create_table
+  sql = <<-SQL CREATE TABLE students(
+    id PRIMARY KEY INTEGER,
+    name TEXT,
+    grade INTEGER
+  )
+  SQL
+ DB[:conn].execute(sql)
+
+end
+
+end
+
